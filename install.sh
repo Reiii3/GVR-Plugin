18557fd612248920e038225d3f38e23460afbed2
Reiii3 <mohtoyib79@gmail.com>
2025-01-24 17:02:59 +0700

    update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ import axeron.prop
 local core="r17rYI0tYD6Cp9pPOtlQ2c0rYMzuOEctdEmseIcseHlP29kC2QyrYAcvaZ1Ez9DPOyctd9lC21yrN4mt2ycsXnmP29pQJ5qrR=="
 
 echo "================================================="
-echo "           GVR PLUGIN INFORMATION"
+echo "             GVR PLUGIN INFORMATION"
 echo "================================================="
 
 echo "Running instalation pleas wait"
