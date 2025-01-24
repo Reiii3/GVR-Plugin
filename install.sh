89521c1299aed8c44e78052d4772e18006488945
Reiii3 <mohtoyib79@gmail.com>
2025-01-24 16:53:43 +0700

    update install.sh

--- install.sh
+++ install.sh
@@ -6,4 +6,13 @@ $AXFUN
 import axeron.prop
 local core="r17rYI0tYD6Cp9pPOtlQ2c0rYMzuOEctdEmseIcseHlP29kC2QyrYAcvaZ1Ez9DPOyctd9lC21yrN4mt2ycsXnmP29pQJ5qrR=="
 
+echo "================================================="
+echo "           GVR PLUGIN INFORMATION"
+echo "================================================="
 
+echo "Running instalation pleas wait"
+sleep 0.5
+echo "running succesfully"
+echo "Open to GVR"
+
+xtorm $core
