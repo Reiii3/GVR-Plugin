e03bcab99294c15d6c32dbd5cee721e6e975c456
Reiii3 <mohtoyib79@gmail.com>
2025-02-11 10:41:47 +0700

    update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ fi
 $AXFUN
 import axeron.prop
 local core="r17rYI0tYD6Cp9pPOtlQ2c0rYMzuOEctdEmseIcseHlP29kC2QyrYAcvaZ1Ez9DPOyctd9lC21yrN4mt2ycsXnmP29pQJ5qrR=="
-runPackage="com.rezone.gvortex"
+runPackage1="com.rezone.gvortex"
 
 if [ -z $runPackage ]; then
     echo "Package is Empty"
